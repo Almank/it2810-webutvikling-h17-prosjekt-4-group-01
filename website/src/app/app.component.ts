44129ba6fe578113827ba30228b3a79187dba306
Petter Lohne <plohne95@gmail.com>
2017-10-24 17:22:09 +0200

    Mer frontend

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,59 +1,46 @@
-import {Component} from "@angular/core";
-export class Movie{
-  id: number;
-  name: string;
-}
+import { Component, OnInit } from '@angular/core';
 
-const MOVIES: Movie[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { Movie } from './movie';
+import { MovieService } from './movie.service';
 
 @Component({
-  selector: 'content-root',
+  selector: 'app-root',
   template: `
     <mat-toolbar color="primary">
       <span class="pageTitle">Top50</span>
       <span class="example-spacer"></span>
       <button mat-button><mat-icon class="profileIcon">account_circle</mat-icon></button>
     </mat-toolbar>
-
-
     <h1>{{title}}</h1>
     <h2>My Movies</h2>
     <ul class="movies">
       <li *ngFor="let movie of movies"
-          [class.selected]="movie === selectedMovie"
-          (click)="onSelect(movie)">
+        [class.selected]="movie === selectedMovie"
+        (click)="onSelect(movie)">
         <span class="badge">{{movie.id}}</span> {{movie.name}}
       </li>
     </ul>
-    <div *ngIf="selectedMovie">
-      <h2>{{selectedMovie.name}} details!</h2>
-      <div><label>id: </label>{{selectedMovie.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedMovie.name" placeholder="name"/>
-      </div>
-    </div>
+    <movie-detail [movie]="selectedMovie"></movie-detail>
+  `,
 
-  `
+  providers: [MovieService]
 })
-
-export class AppComponent {
-  title = 'Movie database';
-  movies = MOVIES;
+export class AppComponent implements OnInit {
+  title = 'Tour of Movies';
+  movies: Movie[];
   selectedMovie: Movie;
+
+  constructor(private movieService: MovieService) { }
+
+  getMovies(): void {
+    this.movieService.getMovies().then(movies => this.movies = movies);
+  }
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
 
   onSelect(movie: Movie): void {
     this.selectedMovie = movie;
-  };
+  }
 }
--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule, MatMenuModule, MatIconModule, MatToolbarModule, MatCardModule  } from '@angular/material';
 import { FormsModule }   from '@angular/forms';
-
+import {MovieDetailComponent} from "./movie-detail.component";
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieDetailComponent
   ],
   imports: [
     BrowserModule,
