92f866d817c65f306d3da60807eb275cfc7b0fa8
martinlunde <martlun@stud.ntnu.no>
2017-10-25 12:36:00 +0200

    add user-schema to models

--- website/models.js
+++ website/models.js
@@ -1,20 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let MovieSchema = new Schema({
-  title: { type: String, default: "undefined"},
-  year: {type: Number, default: 0},
-  runtime: {type: String, default: 'undefined'},
-  genre: {type: String, default: 'undefined'},
-  director: {type: String, default: 'undefined'},
-  actors: {type: String, default: 'undefined'},
-  plot: {type: String, default: 'undefined'},
-  poster: {type: String, default: 'undefined'},
-  readMore: { type: String, default: "undefined"},
+const MovieSchema = new Schema({
+    title: { type: String, default: "undefined"},
+    year: {type: Number, default: 0},
+    runtime: {type: String, default: 'undefined'},
+    genre: {type: String, default: 'undefined'},
+    director: {type: String, default: 'undefined'},
+    actors: {type: String, default: 'undefined'},
+    plot: {type: String, default: 'undefined'},
+    poster: {type: String, default: 'undefined'},
+    readMore: { type: String, default: "undefined"},
 });
 
 //Todo: User, history
+const UserSchema = new Schema({
+    username: { type: String, required: true},
+    password: { type: String, required: true},
+    searchHistory: { type: Array, default: []},
+    favorites: { type: Array, default: []}
+});
 
-let Movie = mongoose.model('Movie', MovieSchema);
+const Movie = mongoose.model('Movie', MovieSchema);
+const User = mongoose.model('User', UserSchema);
 
-module.exports = {Movie};
+module.exports = {Movie, User};
