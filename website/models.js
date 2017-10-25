f1aaa9f92d0fd317ff2b3dbc21f22c59667dda73
martinlunde <martlun@stud.ntnu.no>
2017-10-25 11:38:30 +0200

    add dist to gitignore, and update model to contain more info

--- website/models.js
+++ website/models.js
@@ -2,8 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let MovieSchema = new Schema({
-  link: { type: String, default: "/"},
-  title: { type: String, default: "undefined movie"},
+  title: { type: String, default: "undefined"},
+  year: {type: Number, default: 0},
+  runtime: {type: String, default: 'undefined'},
+  genre: {type: String, default: 'undefined'},
+  director: {type: String, default: 'undefined'},
+  actors: {type: String, default: 'undefined'},
+  plot: {type: String, default: 'undefined'},
+  poster: {type: String, default: 'undefined'},
+  readMore: { type: String, default: "undefined"},
 });
 
 let Movie = mongoose.model('Movie', MovieSchema);
