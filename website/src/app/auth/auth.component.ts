b8dc8110a016681db1098c1a588aeb3670c56fd8
martinlunde <martlun@stud.ntnu.no>
2017-11-01 16:51:49 +0100

    add registration and login for users

--- website/src/app/auth/auth.component.ts
+++ website/src/app/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { isObject } from 'util';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
-
+  onRegister(form) {
+    const params = new HttpParams().set('username', form.value.username).set('password', form.value.password);
+    this.http.post('/api/register', {params}).subscribe(data => {
+      if (isObject(data)) {
+        console.log(data);
+      }
+    });
+  }
+  onLogin(form) {
+    const params = new HttpParams().set('username', form.value.username).set('password', form.value.password);
+    this.http.post('/api/login', {params}).subscribe(data => {
+      if (isObject(data)) {
+        console.log(data);
+      }
+    });
+  }
 }
