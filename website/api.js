260112a817af860be437a42a64762be829a60423
Christoffer Almankaas <chrisa.a87@gmail.com>
2017-11-01 17:56:38 +0100

    Make code less duplicated on actor filter in API.

--- website/api.js
+++ website/api.js
@@ -103,35 +103,30 @@ router.get('/movies', function(req, res) {
     const page = parseInt(req.query.page * 25);
     const limit = parseInt(req.query.limit);
     let year = splitYear(req.query.year);
-    let actors = req.query.actors;
+
     if (year === undefined || req.query.year === ''){
       year = [0,9999]
     }
+
     let genre = splitElements(req.query.genre);
     if (req.query.genre === '' || req.query.genre === undefined){
       genre = categories
     }
 
-    if (actors === undefined || actors === ''){
-      db.collection('movies').find({genre: {$in: genre}, year: {$gte: year[0], $lte: year[1]}}).sort().limit(limit).skip(page).toArray(function(err, docs) {
-        if (err) {
-          handleError(res, err.message, "Failed to get movies with no actors.");
-        } else {
-          res.status(200).json(docs);
-        }
-      });
+    let actors = req.query.actors;
+    if (actors === undefined || actors === '') {
+      actors = {$exists:true};
+    } else {
+      actors = {$in: splitElements(req.query.actors)};
     }
-    else {
-      actors = splitElements(actors);
-      db.collection('movies').find({genre: {$in: genre}, year: {$gte: year[0], $lte: year[1]}, actors: {$in: actors}
-      }).sort().limit(limit).skip(page).toArray(function (err, docs) {
-        if (err) {
-          handleError(res, err.message, "Failed to get movies.");
-        } else {
-          res.status(200).json(docs);
-        }
-      });
-    }
+
+    db.collection('movies').find({genre: {$in: genre}, year: {$gte: year[0], $lte: year[1]}, actors: actors }).sort().limit(limit).skip(page).toArray(function(err, docs) {
+      if (err) {
+        handleError(res, err.message, "Failed to get movies with no actors.");
+      } else {
+        res.status(200).json(docs);
+      }
+    });
 });
 
 // Get movies title ascending
