d78c632d26833b08552c607c95de64de336a92fb
martinlunde <martlun@stud.ntnu.no>
2017-11-01 13:19:10 +0100

    create profile card with tabs for information

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -2,7 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
-import {MatButtonModule, MatDialogModule, MatIconModule, MatInputModule, MatPaginatorModule, MatSidenavModule, MatTableModule, MatToolbarModule, MatTooltipModule,} from '@angular/material';
+import {MatButtonModule, MatDialogModule, MatIconModule, MatInputModule, MatPaginatorModule,
+MatSidenavModule, MatTableModule, MatToolbarModule, MatTooltipModule, MatCardModule, MatTabsModule,
+MatFormFieldModule,
+} from '@angular/material';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
 import {CdkTableModule} from '@angular/cdk/table';
@@ -28,12 +31,15 @@ const appRoutes: Routes = [
     BrowserAnimationsModule,
     CdkTableModule,
     MatButtonModule,
+    MatCardModule,
     MatDialogModule,
+    MatFormFieldModule,
     MatIconModule,
     MatInputModule,
     MatPaginatorModule,
     MatSidenavModule,
     MatTableModule,
+    MatTabsModule,
     MatToolbarModule,
     MatTooltipModule,
     HttpClientModule,
