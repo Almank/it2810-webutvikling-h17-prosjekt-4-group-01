ca018086d025753acdf1952050fa06baa5ee2f9d
martinlunde <martlun@stud.ntnu.no>
2017-11-13 16:26:30 +0100

    make movieList tests pass

--- website/src/app/app.component.spec.ts
+++ website/src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {RouterTestingModule} from '@angular/router/testing';
-import {MatFormFieldModule, MatIconModule, MatInputModule, MatToolbar, MatToolbarRow} from '@angular/material';
+import {MatIconModule, MatToolbar, MatToolbarRow} from '@angular/material';
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -10,7 +10,7 @@ describe('AppComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, MatIconModule, MatFormFieldModule, MatInputModule],
+      imports: [RouterTestingModule, MatIconModule],
       declarations: [AppComponent, HeaderComponent, MatToolbar, MatToolbarRow]
     })
       .compileComponents();
