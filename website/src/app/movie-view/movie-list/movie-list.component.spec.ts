e29496eb82b95a41516190a1f4825346dd5dfb46
martinlunde <martlun@stud.ntnu.no>
2017-11-14 12:57:06 +0100

    make movieListComponent tests pass

--- website/src/app/movie-view/movie-list/movie-list.component.spec.ts
+++ website/src/app/movie-view/movie-list/movie-list.component.spec.ts
@@ -1,6 +1,8 @@
-
 import {TestBed, async, MovieListComponent} from '../../import-module';
-import {MatDialogModule, MatIconModule, MatInputModule, MatPaginatorModule, MatTableModule} from '@angular/material';
+import {
+  MatDialogModule, MatIconModule, MatInputModule, MatOptionModule, MatPaginatorModule,
+  MatTableModule
+} from '@angular/material';
 import {MovieListService} from '../movie-view.service';
 import {HttpClientModule} from '@angular/common/http';
 
@@ -8,7 +10,8 @@ import {HttpClientModule} from '@angular/common/http';
 describe('MovieListComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MatInputModule, MatIconModule, MatTableModule, MatPaginatorModule, MatDialogModule, HttpClientModule],
+      imports: [MatInputModule, MatIconModule, MatTableModule, MatPaginatorModule, MatDialogModule, HttpClientModule,
+        MatOptionModule],
       declarations: [MovieListComponent],
       providers: [{provide: MovieListService}],
     }).compileComponents();
