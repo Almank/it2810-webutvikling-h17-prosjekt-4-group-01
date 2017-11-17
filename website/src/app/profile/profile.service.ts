66126df1348d3bad5a7721fc1706adcb189c5742
martinlunde <martlun@stud.ntnu.no>
2017-11-17 23:36:25 +0100

    make comments and move relevant code to services

--- website/src/app/profile/profile.service.ts
+++ website/src/app/profile/profile.service.ts
@@ -16,36 +16,27 @@ export class ProfileService {
     }
   }
 
-  onLogout() {
-    localStorage.removeItem('session');
-    console.log('session is removed');
-    this.router.navigate(['/']);
-    location.reload();
+  // Handles the http post request to change the password.
+  onNewPassword(form) {
+    const params = JSON.stringify({
+      token: this.token,
+      oldPassword: form.value.oldPassword,
+      newPassword: form.value.newPassword
+    });
+    return this.http.post('/api/new_password', params, {headers: this.headers})
+      .toPromise()
+      .then( data => {
+      return data;
+    });
   }
 
+  // Handles the http post request to validate the jwt token.
   validateToken(token) {
     const params = JSON.stringify({
       token: token,
     });
-    this.http.post('/api/login/verify', params, {headers: this.headers}).toPromise().then(data => {
-    }, err => {
-      if (isObject(err)) {
-        this.onUserAlert(err.error.message, 'dismiss', false, 4000);
-        this.onLogout();
-      }
+    return this.http.post('/api/login/verify', params, {headers: this.headers}).toPromise().then( data => {
+      return data;
     });
   }
-
-
-  onUserAlert(message: string, action: string, positive: boolean, duration = 2000) {
-    let extra = 'alert-negative';
-    if (positive) {
-      extra = 'alert-positive';
-    }
-    this.snackBar.open(message, action, {
-      extraClasses: [extra],
-      duration: duration
-    });
-  }
-
 }
