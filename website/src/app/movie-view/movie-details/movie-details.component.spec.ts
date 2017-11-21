7316b1b82a4df61e1051316de199ecf489a9e582
Christoffer Almankaas <chrisa.a87@gmail.com>
2017-11-21 14:44:45 +0100

    Add tests for movie-details

--- website/src/app/movie-view/movie-details/movie-details.component.spec.ts
+++ website/src/app/movie-view/movie-details/movie-details.component.spec.ts
@@ -2,10 +2,15 @@ import {async, ComponentFixture, TestBed, MovieDetailsComponent, MAT_DIALOG_DATA
   MatFormFieldModule, CommonModule, Favorite, RouterTestingModule, FormsModule, HttpClientModule
 } from '../../import-module';
 
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
 
 describe('MovieDetailsComponent', () => {
   let component: MovieDetailsComponent;
   let fixture: ComponentFixture<MovieDetailsComponent>;
+  let debugElement: DebugElement[];
+  let htmlElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -37,5 +42,61 @@ describe('MovieDetailsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display director or actors', () => {
+    debugElement = fixture.debugElement.queryAll(By.css('.infoType'));
+    for (let i = 0; i < debugElement.length; i++) {
+      htmlElement = debugElement[i].nativeElement;
+      expect(['Director:', 'Actors:']).toContain(htmlElement.textContent);
+    }
+  });
+
+  it('should display cancel or stars', () => {
+    debugElement = fixture.debugElement.queryAll(By.css('.favIcon'));
+    for (let i = 0; i < debugElement.length; i++) {
+      htmlElement = debugElement[i].nativeElement;
+      expect(['stars', 'cancel']).toContain(htmlElement.textContent);
+    }
+  });
+
+  it('should display subtitle', () => {
+    debugElement = fixture.debugElement.queryAll(By.css('.subTitle'));
+    for (let i = 0; i < debugElement.length; i++) {
+      htmlElement = debugElement[i].nativeElement;
+      expect(htmlElement.textContent).toContain('Movie / ' + component.data.runtime + ' / ' + component.data.genre);
+    }
+  });
+
+  it('should display title', () => {
+    debugElement = fixture.debugElement.queryAll(By.css('.titleContainer h2'));
+    for (let i = 0; i < debugElement.length; i++) {
+      htmlElement = debugElement[i].nativeElement;
+      expect(htmlElement.textContent).toContain(component.data.title + ' (' + component.data.year + ')');
+    }
+  });
+
+  it('should display readmore', () => {
+    debugElement = fixture.debugElement.queryAll(By.css('.readMore'));
+    for (let i = 0; i < debugElement.length; i++) {
+      htmlElement = debugElement[i].nativeElement;
+      expect(htmlElement.textContent).toContain('Read more');
+    }
+  });
+
+  it('should display actor or director name', () => {
+    debugElement = fixture.debugElement.queryAll(By.css('.infoDetails'));
+    for (let i = 0; i < debugElement.length; i++) {
+      htmlElement = debugElement[i].nativeElement;
+      expect([component.data.director, component.data.actors]).toContain(htmlElement.textContent);
+    }
+  });
+
+  it('should display movie plot', () => {
+    debugElement = fixture.debugElement.queryAll(By.css('.modalContent p'));
+    for (let i = 0; i < debugElement.length; i++) {
+      htmlElement = debugElement[i].nativeElement;
+      expect(htmlElement.textContent).toEqual(component.data.plot);
+    }
   });
 });
