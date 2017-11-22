01dc12e5e71c0819262237bc065acbc6152e3d28
steffhe <steffhe@stud.ntnu.no>
2017-11-22 17:36:47 +0100

    wordcloud and profile tests

--- website/src/app/profile/profile.component.spec.ts
+++ website/src/app/profile/profile.component.spec.ts
@@ -6,10 +6,14 @@ import {MovieDetailsService} from '../movie-view/movie-details/movie-details.ser
 import {MovieListService} from '../movie-view/movie-view.service';
 import {ProfileService} from './profile.service';
 import {ProfileHistoryService} from './profile.history.service';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -24,10 +28,44 @@ describe('ProfileComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
+    //Mock-values
+    component.username = 'test';
+    component.favoriteListData = [
+        {"_id":"5a1424524bc8d140dcfe1ede","poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg","actors":["Leonardo DiCaprio","Joseph Gordon-Levitt","Ellen Page","Tom Hardy"],"director":["Christopher Nolan"],"genre":["Action","Adventure","Sci-Fi"],"year":2010,"title":"Inception"}
+    ];
+    component.history.currentHistory = [
+        {"_id":"5a1424534bc8d140dcfe22fa","poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMWU4N2FjNzYtNTVkNC00NzQ0LTg0MjAtYTJlMjFhNGUxZDFmXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg","actors":["Martin Balsam","John Fiedler","Lee J. Cobb","E.G. Marshall"],"director":["Sidney Lumet"],"genre":["Crime","Drama"],"year":1957,"title":"12 Angry Men"}
+    ];
+
     fixture.detectChanges();
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display actual username', () => {
+    debugElement = fixture.debugElement.query(By.css('#username'));
+    htmlElement = debugElement.nativeElement;
+    expect(htmlElement.textContent).toEqual('test');
+  });
+
+  it('should display Logout-button properly', () => {
+    debugElement = fixture.debugElement.query(By.css('.logout-button'));
+    htmlElement = debugElement.nativeElement;
+    expect(htmlElement.textContent).toEqual('Logout');
+  });
+
+  it('should display favorite movie title', () => {
+    debugElement = fixture.debugElement.query(By.css('#gridTitleFav'));
+    htmlElement = debugElement.nativeElement;
+    expect(htmlElement.textContent).toEqual('Inception');
+  });
+  it('should display search history movie title', () => {
+    debugElement = fixture.debugElement.query(By.css('#gridTitleSearch'));
+    htmlElement = debugElement.nativeElement;
+    expect(htmlElement.textContent).toEqual('12 Angry Men');
+  });
+
 });
