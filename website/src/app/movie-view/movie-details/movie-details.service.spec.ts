1eb385d0125237b48fd3a6d087f3c56302784197
Christoffer Almankaas <chrisa.a87@gmail.com>
2017-11-22 16:36:17 +0100

    Add test to movie details service.

---
+++ website/src/app/movie-view/movie-details/movie-details.service.spec.ts
@@ -0,0 +1,68 @@
+import {async, Component, ComponentFixture, TestBed, MAT_DIALOG_DATA, MatDialogModule, MatDialogRef,
+  MatFormFieldModule, CommonModule, RouterTestingModule, FormsModule, HttpClientModule, MovieDetailsService
+} from '../../import-module';
+
+import { getTestBed, inject } from '@angular/core/testing';
+import { XHRBackend, BaseRequestOptions, HttpModule, Http, Response, ResponseOptions } from '@angular/http';
+import {MockBackend, MockConnection} from '@angular/http/testing';
+
+  class MockMovieDetailsService {
+    openDialog(data, auth, token) {
+      if (auth) {
+        const params = JSON.stringify({
+          token: token,
+          movie_id: data._id,
+        });
+      }
+    }
+  }
+
+  describe('Movie Details Service', () => {
+    let mockBackend: MockBackend;
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          MockBackend,
+          MockMovieDetailsService,
+          BaseRequestOptions,
+          {
+            provide: Http,
+            deps: [MockBackend, MockMovieDetailsService],
+            useFactory:
+              (backend: XHRBackend, defaultOptions: BaseRequestOptions) => {
+                return new Http(backend, defaultOptions);
+              }
+          }
+        ],
+        imports: [
+          FormsModule,
+          HttpModule
+        ]
+      });
+      mockBackend = getTestBed().get(MockBackend);
+    }));
+
+    it('Should be run correctly', done => {
+      let mockMovieDetailsService: MockMovieDetailsService;
+
+      getTestBed().compileComponents().then(() => {
+        mockBackend.connections.subscribe(
+          (connection: MockConnection) => {
+            connection.mockRespond(new Response(
+              new ResponseOptions({
+                  body: [
+                    {
+                      id: 26,
+                      contentRendered: '<p>Test Text</p>',
+                      contentMarkdown: '*Test Text*'
+                    }]
+                }
+              )));
+          });
+
+        mockMovieDetailsService = getTestBed().get(MockMovieDetailsService);
+        expect(mockMovieDetailsService).toBeDefined();
+        done();
+      });
+    });
+  });
